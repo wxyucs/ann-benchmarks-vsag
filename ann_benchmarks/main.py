b4a0eb42cfe5f8275a5e796b3d09d94f88b7d747
Xiangyu Wang <wxy407827@antgroup.com>
2024-10-15 17:25:11 +0800

    add vsag library

--- ann_benchmarks/main.py
+++ ann_benchmarks/main.py
@@ -70,7 +70,7 @@ def run_worker(cpu: int, args: argparse.Namespace, queue: multiprocessing.Queue)
             run(definition, args.dataset, args.count, args.runs, args.batch)
         else:
             memory_margin = 500e6  # reserve some extra memory for misc stuff
-            mem_limit = int((psutil.virtual_memory().available - memory_margin) / args.parallelism)
+            mem_limit = int((psutil.virtual_memory().total - memory_margin) / args.parallelism)
             cpu_limit = str(cpu) if not args.batch else f"0-{multiprocessing.cpu_count() - 1}"
             
             run_docker(definition, args.dataset, args.count, args.runs, args.timeout, args.batch, cpu_limit, mem_limit)
